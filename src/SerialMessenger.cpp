9af6cf201a4f364f9cf471b45d84df16b010e60e
Eric Mackrodt <ericmackrodt@Loaners-MacBook-Pro.local>
2018-04-10 01:05:04 +1000

    Implement subscribing to messages.

--- src/SerialMessenger.cpp
+++ src/SerialMessenger.cpp
@@ -1,24 +1,111 @@
 #include <Arduino.h>
 #include <SerialMessenger.h>
-#include <vector>
-
-#ifdef SoftwareSerial_h
 #include <SoftwareSerial.h>
-#endif
 
-SerialMessenger::SerialMessenger() {}
+SerialMessenger::SerialMessenger() {
+  _isHardwareSerial = true;
+}
 
-#ifdef SoftwareSerial_h
-SerialMessenger::SerialMessenger(SoftwareSerial * serial) {
+SerialMessenger::SerialMessenger(SoftwareSerial* serial) {
   _softSerial = serial;
-}
-#endif 
+  _isHardwareSerial = false;
+} 
 
-SerialMessenger::subscribe(String channel, void(*func)(String)) {
-  int index = _channels.indexOf(channel);
-  _channelFunctions[index] = func;
+int SerialMessenger::subscribe(String channel, void (*func)(String)) {
+  _channels[_channelsCount] = channel;
+  _chanelFunctions[_channelsCount] = func;
+  _channelsCount++;
+  return _channelsCount - 1;
 }
 
-SerialMessenger::publish(String channel, String message) {
+void SerialMessenger::setPrefix(char prefix) {
+  _prefix = prefix;
+}
+
+void SerialMessenger::setSuffix(char suffix) {
+  _suffix = suffix;
+}
+
+void SerialMessenger::setDelimiter(char delimiter) {
+  _delimiter = delimiter;
+}
+
+void SerialMessenger::send(String channel, String message) {
   
+}
+
+void SerialMessenger::handle() {
+  if (_received()) {
+    for (int i = 0; i < _channelsCount; i++) {
+      if (_isChannel(_channels[i])) {
+        _chanelFunctions[i](_getParameter());
+      }
+    }
+  }
+}
+
+// Private Functions
+
+bool SerialMessenger::_serialAvailable() {
+  if (_isHardwareSerial == true) {
+    return Serial.available();
+  } else {
+    return _softSerial->available();
+  }
+}
+
+int SerialMessenger::_read() {
+  if (_isHardwareSerial == true) {
+    return Serial.read();
+  } else {
+    return _softSerial->read();
+  }
+}
+
+bool SerialMessenger::_received() {
+  if(_serialAvailable()) {
+      char read = char(_read());
+
+      if (read == _prefix) {
+        _isReadingMessage = true;
+        return false;
+      } else if(read == _suffix) {
+        _message  = _buffer;
+        _buffer = "";
+        _isReadingMessage = false;
+        return true;
+      } else if (_isReadingMessage == true) {
+        _buffer += read;
+        return false;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+}
+
+String SerialMessenger::_getChannel() {
+  int delimiter = _message.indexOf(_delimiter);
+
+  if (delimiter > -1) {
+    return _message.substring(0, delimiter);
+  } else {
+    return _message;
+  }
+}
+
+String SerialMessenger::_getParameter() {
+  int delimiter = _message.indexOf(_delimiter);
+  int end = _message.length();
+
+  if (delimiter > -1 && end > delimiter) {
+    return _message.substring(delimiter + 1, end);
+  } else {
+    return "";
+  }
+}
+
+bool SerialMessenger::_isChannel(String channel) {
+  return _getChannel() == channel;
 }
--- src/SerialMessenger.h
+++ src/SerialMessenger.h
@@ -7,29 +7,46 @@
 #define SerialMessenger_h
 
 #include "Arduino.h"
+#include <SoftwareSerial.h>
 
-#ifdef SoftwareSerial_h
-#include "SoftwareSerial.h"
+#ifdef SERIALMESSENGER_EVENTS_NUMBER
+#define NUMBER_EVENTS SERIALMESSENGER_EVENTS_NUMBER
+#endif
+
+#ifndef NUMBER_EVENTS
+#define NUMBER_EVENTS 5
 #endif
 
 class SerialMessenger
 {
   public:
-    #ifdef SoftwareSerial_h
-    SerialMessenger(String channels[], SoftwareSerial * serial);
-    #endif
-    SerialMessenger(String channels[]);
-    void subscribe(String channel, void (*func)(String));
+    SerialMessenger();
+    SerialMessenger(SoftwareSerial* serial);
+    int subscribe(String channel, void (*func)(String));
     void send(String channel, String message);
     void setPrefix(char prefix);
     void setSuffix(char suffix);
     void setDelimiter(char delimiter);
     void handle();
-    ~SerialMessenger();
   private:
-    String _channels[];
-    (void (*func)(String)) _channelFunctions[];
-    SoftwareSerial * _softSerial;
+    bool _isHardwareSerial;
+    bool _isReadingMessage;
+    String _channels[NUMBER_EVENTS];
+    void (*_chanelFunctions[NUMBER_EVENTS])(String);
+    int _channelsCount;
+    char _prefix = '^';
+    char _suffix = '$';
+    char _delimiter = '#';
+    String _buffer = "";
+    String _message = "";
+    SoftwareSerial* _softSerial;
+    // Private Functions
+    bool _serialAvailable();
+    bool _received();
+    String _getChannel();
+    String _getParameter();
+    bool _isChannel(String channel);
+    int _read();
 };
 
 #endif
